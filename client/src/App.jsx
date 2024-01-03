316df06f81a5b1cb6d885c00677c39720a186f52
Chayutp <135034854+Chayutp@users.noreply.github.com>
2024-01-03 18:43:15 +0700

    feat:context-phum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { nameContext } from "./context/NameContext.jsx";
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
@@ -12,14 +13,20 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
-    </div>
+    <nameContext.Provider
+      value={{
+        userData,
+      }}
+    >
+      <div className="App">
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </nameContext.Provider>
   );
 }
 
--- client/src/components/AppHeader.jsx
+++ client/src/components/AppHeader.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
+import { nameContext } from "../context/NameContext";
+
 function AppHeader() {
+  const infoContext = useContext(nameContext);
   return (
     <div className="app-header-container">
       <h1 className="app-title">Products</h1>
-      <h2>สวัสดีคุณ (x)</h2>
+      <h2>สวัสดีคุณ {infoContext.userData.username}</h2>
     </div>
   );
 }
